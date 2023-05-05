89f2a6ba1673a2eb025c969b158a0ad1cb511544
alexxShandsome <algilbertgomez@gmail.com>
2023-05-05 08:30:17 -0700

    mysql connection module

---
+++ MyProject/Module_public.vb
@@ -0,0 +1,29 @@
+﻿Imports MySql.Data.MySqlClient
+Module Module_public
+    Public myconn As New MySqlConnection
+    Public myConnectionString As String
+    Public strSQL As String
+
+    Public Sub Connect_to_DB()
+        myConnectionString = "server=127.0.0.1;" _
+                    & "uid=al;" _
+                    & "pwd=hello;" _
+                    & "database=edp_database"
+        Try
+            myconn.ConnectionString = myConnectionString
+            myconn.Open()
+        Catch ex As MySqlException
+            Select Case ex.Number
+                Case 0
+                    MsgBox("Cannot Connect to Server")
+                Case 1045
+                    MsgBox("Invalid Username or password")
+            End Select
+
+        End Try
+    End Sub
+    Public Sub Disconnect_to_DB()
+        myconn.Close()
+        myconn.Dispose()
+    End Sub
+End Module
