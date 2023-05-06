c727d87b67f92be5d9a30f2a11af0909faf78b38
alexxShandsome <algilbertgomez@gmail.com>
2023-05-06 07:23:43 -0700

    login: working login form

--- MyProject/Form_Login.Designer.vb
+++ MyProject/Form_Login.Designer.vb
@@ -22,17 +22,86 @@ Partial Class Form_Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.LoginBtn = New System.Windows.Forms.Button()
+        Me.PasswordTextBox = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.UsernameTextBox = New System.Windows.Forms.TextBox()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'LoginBtn
+        '
+        Me.LoginBtn.Location = New System.Drawing.Point(127, 314)
+        Me.LoginBtn.Name = "LoginBtn"
+        Me.LoginBtn.Size = New System.Drawing.Size(198, 30)
+        Me.LoginBtn.TabIndex = 0
+        Me.LoginBtn.Text = "Login"
+        Me.LoginBtn.UseVisualStyleBackColor = True
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Location = New System.Drawing.Point(99, 211)
+        Me.PasswordTextBox.Multiline = True
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.Size = New System.Drawing.Size(248, 44)
+        Me.PasswordTextBox.TabIndex = 1
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'UsernameTextBox
+        '
+        Me.UsernameTextBox.Location = New System.Drawing.Point(99, 122)
+        Me.UsernameTextBox.Multiline = True
+        Me.UsernameTextBox.Name = "UsernameTextBox"
+        Me.UsernameTextBox.Size = New System.Drawing.Size(248, 44)
+        Me.UsernameTextBox.TabIndex = 3
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UsernameLabel.Location = New System.Drawing.Point(107, 85)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(70, 16)
+        Me.UsernameLabel.TabIndex = 5
+        Me.UsernameLabel.Text = "Username"
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PasswordLabel.Location = New System.Drawing.Point(107, 180)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(67, 16)
+        Me.PasswordLabel.TabIndex = 6
+        Me.PasswordLabel.Text = "Password"
         '
         'Form_Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PasswordLabel)
+        Me.Controls.Add(Me.UsernameLabel)
+        Me.Controls.Add(Me.UsernameTextBox)
+        Me.Controls.Add(Me.PasswordTextBox)
+        Me.Controls.Add(Me.LoginBtn)
         Me.Name = "Form_Login"
         Me.Text = "Login"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents LoginBtn As Button
+    Friend WithEvents PasswordTextBox As TextBox
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents UsernameTextBox As TextBox
+    Friend WithEvents UsernameLabel As Label
+    Friend WithEvents PasswordLabel As Label
 End Class
--- MyProject/Form_Login.vb
+++ MyProject/Form_Login.vb
@@ -1,3 +1,31 @@
-﻿Public Class Form_Login
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Form_Login
+    Private Sub Login_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoginBtn.Click
+        With Me
+            Call Connect_to_DB()
+            Dim mycmd As New MySqlCommand
+            Dim myreader As MySqlDataReader
+
+
+            strSQL = "Select * from registered_users where USER_NAME = '" _
+                & .UsernameTextBox.Text & "' and PASSWORD = '" _
+                & .PasswordTextBox.Text & "'"
+            'MsgBox(strSQL)
+            mycmd.CommandText = strSQL
+            mycmd.Connection = myconn
+
+            myreader = mycmd.ExecuteReader
+            If myreader.HasRows Then
+                MsgBox("Hello " & .UsernameTextBox.Text)
+            Else
+                MessageBox.Show("Invalid username or password")
+            End If
+            Call Disconnect_to_DB()
+        End With
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles UsernameLabel.Click
+
+    End Sub
 End Class
